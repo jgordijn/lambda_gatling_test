c1c383007d728b8013b72bd87fd96a4aa88dd886
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2016-03-25 16:27:44 +0100

    incl logs

--- src/test/scala/nl/trivento/lambda/LambdaGatlingTest.scala
+++ src/test/scala/nl/trivento/lambda/LambdaGatlingTest.scala
@@ -2,10 +2,14 @@ package nl.trivento.lambda
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import io.gatling.http.request.{ExtraInfo, ExtraInfoExtractor}
 import scala.concurrent.duration._
 
 class LambdaGatlingTest extends Simulation {
-  val conf = http.baseURL("https://rk9wlzrsqb.execute-api.eu-west-1.amazonaws.com/prod")
+  val conf = http.baseURL("https://rk9wlzrsqb.execute-api.eu-west-1.amazonaws.com/prod").extraInfoExtractor(new ExtraInfoExtractor {
+    override def apply(v1: ExtraInfo): List[Any] = {println(s">>>>>>>>>> ${v1.response.body.string}"); List(s">>>>>>>>>> ${v1.response.body.string}")}
+  })
+
 
   val javascn = scenario("Java Test")
         .exec(http("java lambda")
@@ -15,7 +19,9 @@ class LambdaGatlingTest extends Simulation {
         .get("/node"))
 
 
-  setUp(nodescn.inject(constantUsersPerSec(25) during (10 minutes)),
-    javascn.inject(constantUsersPerSec(25) during (10 minutes)))
+//  setUp(nodescn.inject(constantUsersPerSec(25) during (60 seconds)),
+//    javascn.inject(constantUsersPerSec(25) during (60 seconds)))
+//        .protocols(conf)
+  setUp(javascn.inject(constantUsersPerSec(100) during (60 seconds)))
         .protocols(conf)
 }
